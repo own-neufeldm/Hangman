3c7405ae163e1db9039f57f7799ea007e63a061c
Neufeld-Martin <m.neufeld946@gmail.com>
2022-01-24 21:13:11 +0000

    Define basic functions

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,24 +1,76 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * TODO - comment
+ * 
+ * @author Neufeld-Martin
+ */
 public class Hangman {
 
+  // TODO - comment
+  private static String wordToGuess = null;
+
+  // TODO - comment
+  private static String guessStatus = null;
+
+  // TODO - comment
+  private static List<Character> guessedChars = new ArrayList<>();
+
+  // TODO - comment
+  private static int wrongGuesses = 0;
+
+  // TODO - comment
+  private static int maxGuesses = 5;
+
+  /**
+   * TODO - comment
+   * 
+   * @param args
+   */
   public static void main(String[] args) {
     try(
       Scanner scn = new Scanner(System.in);
     ) {
-      String wordToGuess = promptWord(scn);
-      List<Character> guessedChars = new ArrayList<>();
-      int wrongGuesses = 0;
+      wordToGuess = determineWord(scn);
+      guessStatus = wordToGuess.replaceAll(".", "_");
+
+      for(int curNdx = 0; curNdx < 100; curNdx++) {
+        System.out.println();
+      }
 
       while(true) {
-        Character guessedChar = promptChar(scn, guessedChars);
+        printStats();
+        letPlayerGuess(scn);
+        printSeparator();
+
+        if(guessStatus.equals(wordToGuess)) {
+          System.out.println("Congratulations, you won!");
+          break;
+        }
+
+        if(wrongGuesses > maxGuesses) {
+          System.out.println("Oh no, you lost!");
+          break;
+        }
       }
     }
+
+    System.out.printf("The word to guess was: %s", wordToGuess);
+
+    System.out.println();
+    System.out.println("*** DONE ***");
   }
 
-  private static String promptWord(Scanner scn) {
+  /**
+   * TODO - comment
+   * 
+   * @param scn
+   * @return
+   */
+  private static String determineWord(Scanner scn) {
     String wordToGuess = null;
     
     while(wordToGuess == null) {
@@ -30,20 +82,47 @@ public class Hangman {
         System.out.println("Input is invalid. Your word must only consist of letters and contain at least one letter.");
       }
     }
-    
+
     return wordToGuess;
   }
 
+  /**
+   * TODO - comment
+   * 
+   * @param chr
+   * @return
+   */
+  private static boolean isValidChar(Character chr) {
+    if(chr == null) {
+      return false;
+    }
+
+    return Character.isLetter(chr);
+  }
+
+  /**
+   * TODO - comment
+   * 
+   * @param word
+   * @return
+   */
   private static boolean isValidWord(String word) {
     if(word == null) {
       return false;
     }
 
-    return word.matches("[A-Z]");
+    return word.matches("[A-Z]+");
   }
 
-  private static Character promptChar(Scanner scn, List<Character> guessedChars) {
+  /**
+   * TODO - comment
+   * 
+   * @param scn
+   */
+  private static void letPlayerGuess(Scanner scn) {
     Character guessedChar = null;
+    StringBuilder guessStatusBuilder = new StringBuilder(guessStatus);
+    int replacedChars = 0;
     
     while(guessedChar == null) {
       System.out.print("Please guess a letter: ");
@@ -60,18 +139,48 @@ public class Hangman {
       }
     }
 
-    return guessedChar;
-  }
+    guessedChars.add(guessedChar);
+    guessedChars.sort(Comparator.naturalOrder());
 
-  private static boolean isValidChar(Character chr) {
-    if(chr == null) {
-      return false;
+    for(int curNdx = 0; curNdx < wordToGuess.length(); curNdx++) {
+      if(wordToGuess.charAt(curNdx) == guessedChar) {
+        guessStatusBuilder.setCharAt(curNdx, guessedChar);
+        replacedChars++;
+      }
     }
 
-    return Character.isLetter(chr);
+    if(replacedChars == 0) {
+      wrongGuesses++;
+    } else {
+      guessStatus = guessStatusBuilder.toString();
+    }
   }
 
-  private static void draw() {
+  /**
+   * TODO - comment 
+   */
+  private static void printSeparator() {
+    System.out.printf("%n# %s #%n%n", "-".repeat(46));
+  }
 
+  /**
+   * TODO - comment
+   */
+  private static void printStats() {
+    System.out.printf("Previous guesses: %s%n", guessedChars.toString());
+    System.out.printf("Wrong guesses: %d (%d)%n", wrongGuesses, maxGuesses);
+    System.out.println();
+
+    System.out.printf("Word to guess (%d): ", wordToGuess.length());
+    for(int curNdx = 0; curNdx < guessStatus.length(); curNdx++) {
+      System.out.print(guessStatus.charAt(curNdx));
+
+      if(curNdx != (guessStatus.length() - 1)) {
+        System.out.print(" ");
+      }
+    }
+    System.out.println();
+
+    // TODO - print Hangman
   }
 }

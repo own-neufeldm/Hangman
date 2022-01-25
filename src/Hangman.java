8a61232dfa6f1ac3169de161ceea5a7528114174
Neufeld-Martin <m.neufeld946@gmail.com>
2022-01-25 21:57:35 +0000

    Add comments

--- src/Hangman.java
+++ src/Hangman.java
@@ -4,37 +4,55 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
- * TODO - comment
+ * This class serves the purpose of playing a game of Hangman in a Terminal.
  * 
  * @author Neufeld-Martin
  */
 public class Hangman {
 
-  // TODO - comment
+  /**
+   * The word to be guessed. Only letters A-Z are supported.
+   * <p>
+   * Whitespaces will be trimmed and the word will be converted to upper case.
+   *   
+   */
   private static String wordToGuess = null;
 
-  // TODO - comment
+  /**
+   * The word to guess with the correctly guessed characters revealed.
+   * <p>
+   * Characters that are not yet guessed will be replaced by an underscore.
+   */
   private static String guessStatus = null;
 
-  // TODO - comment
+  /**
+   * All guessed characters.
+   * <p>
+   * Serves as reference for the guessing player to know which guesses were already made.
+   */
   private static List<Character> guessedChars = new ArrayList<>();
 
-  // TODO - comment
+  /**
+   * Current amount of wrong guesses.
+   */
   private static int wrongGuesses = 0;
 
-  // TODO - comment
+  /**
+   * Maximum allowed amount of wrong guesses.
+   */
   private static int maxGuesses = 5;
 
   /**
-   * TODO - comment
+   * Executes the game.
    * 
-   * @param args
+   * @param args Command-line arguments.
    */
   public static void main(String[] args) {
     try(
+      // Scanner for command-line input.
       Scanner scn = new Scanner(System.in);
     ) {
-      wordToGuess = determineWord(scn);
+      wordToGuess = getWord(scn);
       guessStatus = wordToGuess.replaceAll(".", "_");
 
       for(int curNdx = 0; curNdx < 100; curNdx++) {
@@ -65,12 +83,12 @@ public class Hangman {
   }
 
   /**
-   * TODO - comment
+   * Retrieves {@link #wordToGuess} from player one via command-line.
    * 
-   * @param scn
-   * @return
+   * @param scn Scanner for command-line input.
+   * @return {@link #wordToGuess}.
    */
-  private static String determineWord(Scanner scn) {
+  private static String getWord(Scanner scn) {
     String wordToGuess = null;
     
     while(wordToGuess == null) {
@@ -87,10 +105,12 @@ public class Hangman {
   }
 
   /**
-   * TODO - comment
+   * Checks if the provided character is valid in the context of this game.
+   * <p>
+   * A guessed character is valid if it is a letter between A-Z.
    * 
-   * @param chr
-   * @return
+   * @param chr Character to check.
+   * @return {@code true} if the provided character is valid, {@code false} otherwise.
    */
   private static boolean isValidChar(Character chr) {
     if(chr == null) {
@@ -101,10 +121,12 @@ public class Hangman {
   }
 
   /**
-   * TODO - comment
+   * Checks if the provided word is valid in the context of this game.
+   * <p>
+   * A provided word is valid if it contains only letter between A-Z and has length greater than or equal to 1.
    * 
-   * @param word
-   * @return
+   * @param chr Word to check.
+   * @return {@code true} if the provided character is valid, {@code false} otherwise.
    */
   private static boolean isValidWord(String word) {
     if(word == null) {
@@ -115,9 +137,14 @@ public class Hangman {
   }
 
   /**
-   * TODO - comment
+   * Prompts player two to guess a character.
+   * <p>
+   * The guessed character must be {@link #isValidChar(Character) valid} and must not be guessed already.
+   * <p>
+   * This function is also responsible for updating the fields
+   * {@link #guessedChars}, {@link #wrongGuesses} and {@link #guessStatus} if necessary.
    * 
-   * @param scn
+   * @param scn Scanner for command-line input.
    */
   private static void letPlayerGuess(Scanner scn) {
     Character guessedChar = null;
@@ -157,14 +184,14 @@ public class Hangman {
   }
 
   /**
-   * TODO - comment 
+   * Prints a separator to the Terminal for increased readability of the game. 
    */
   private static void printSeparator() {
     System.out.printf("%n# %s #%n%n", "-".repeat(46));
   }
 
   /**
-   * TODO - comment
+   * Prints information about the current round to the Terminal for the guessing player.
    */
   private static void printStats() {
     System.out.printf("Previous guesses: %s%n", guessedChars.toString());
@@ -180,7 +207,7 @@ public class Hangman {
       }
     }
     System.out.println();
-
-    // TODO - print Hangman
   }
+
+  // TODO - print hanged man
 }

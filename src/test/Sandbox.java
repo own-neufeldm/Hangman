ee164c23317e74a07f4d756cbb863dc52b37ed4d
Neufeld-Martin <m.neufeld946@gmail.com>
2022-03-07 00:10:20 +0000

    Reset Sandbox.java

--- src/test/Sandbox.java
+++ src/test/Sandbox.java
@@ -2,9 +2,6 @@ package test;
 
 public class Sandbox {
   public static void main(String[] args) throws Exception {
-    for (int i = 1; i <= 5; i++) {
-      System.out.printf("%d/5%n", i);
-      Thread.sleep(1000L);
-    }
+    System.out.println();
   }
 }
